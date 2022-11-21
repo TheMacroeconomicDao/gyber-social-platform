e61818ee28a9070d3a6568db3af4941be90487e3
rozoomcool <49102042+rozoomcool@users.noreply.github.com>
2022-11-21 19:07:04 +0300

    Create RequestObjectController.java

---
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/tcpip/RequestObjectController.java
@@ -0,0 +1,9 @@
+package tcpip;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class RequestObjectController {
+}
