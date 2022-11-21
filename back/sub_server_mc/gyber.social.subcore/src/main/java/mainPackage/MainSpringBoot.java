0bddfc6eca083e8f8e1ee32a8a026a4f9f23deea
Nicki <casperplay45@gmail.com>
2022-11-21 13:40:50 +0200

    zero commit

---
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/mainPackage/MainSpringBoot.java
@@ -0,0 +1,13 @@
+package mainPackage;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class MainSpringBoot {
+    public static void main(String[] args) {
+        // ...
+    }
+}
