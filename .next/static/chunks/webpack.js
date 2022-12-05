f0ec5b21d0406dc312fadbf33e16e11ec36a5044
Artyom Narshinov <narshinovartyom@mail.ru>
2022-12-05 21:37:44 +0400

    ui footer for landing layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dc07e78115f4b4c1"; }
+/******/ 		__webpack_require__.h = function() { return "2f77974a027d6425"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
