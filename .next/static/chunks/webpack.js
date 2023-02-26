202e45138759497616f7e94c8cbb5f961e06984c
Tim <TimF1234@yandex.ru>
2023-02-26 17:16:06 +0300

    editing dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ba1e432dfe0a03e"; }
+/******/ 		__webpack_require__.h = function() { return "023ed933ff78a18b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
