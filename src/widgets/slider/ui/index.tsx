1707196c570747a0d200bca894d0af2e4a13bd7d
Tim <TimF1234@yandex.ru>
2023-05-28 11:28:15 +0300

    ref title in slider

--- src/widgets/slider/ui/index.tsx
+++ src/widgets/slider/ui/index.tsx
@@ -1,13 +1,14 @@
 'use client'
 import React, { FC } from 'react'
+import { FTitle } from 'shared/ui/titles/FTitle'
 import styles from './styles.module.scss'
 
 export const Slider:FC = () => {
   return (
     <>
-        <h1 className={styles.header}>
-            AiC (Artificial Intelligence and Blockchain Community)
-        </h1>
+        <FTitle 
+          text='AiC (Artificial Intelligence and Blockchain Community)'
+        />
         <div className={styles.sliderWrap}>
           <div className={styles.container}>
             Slider
