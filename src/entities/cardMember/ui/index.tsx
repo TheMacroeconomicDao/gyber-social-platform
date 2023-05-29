be476f474899e38059d73a32d070be110a17ba12
Tim <TimF1234@yandex.ru>
2023-05-29 22:03:49 +0300

    add pictures in the card

--- src/entities/cardMember/ui/index.tsx
+++ src/entities/cardMember/ui/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Image from "next/image";
 import styles from "./styles.module.scss";
 
 type CardMemberProps = {};
@@ -6,7 +7,15 @@ type CardMemberProps = {};
 export const CardMember: FC<CardMemberProps> = () => {
 	return (
 		<div className={styles.wrap}>
-			<div className={styles.avatar}></div>
+			<div className={styles.avatar}>
+				<Image
+					src="/otto.png" 
+					width={150}
+					height={190}
+					sizes="(max-width: 768px) 100vw"
+					alt="Picture of the author"
+				/>
+			</div>
 			<div className={styles.name}>Otto Kustler</div>
 			<div className={styles.skills}>
 				JS/TS/React/
