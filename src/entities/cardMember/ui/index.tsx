4a33b90985fe15563b6e0774f1aa104cb32e76e4
Tim <TimF1234@yandex.ru>
2023-05-30 21:58:52 +0300

    fix avatars

--- src/entities/cardMember/ui/index.tsx
+++ src/entities/cardMember/ui/index.tsx
@@ -8,14 +8,13 @@ export const CardMember: FC<CardMemberProps> = () => {
 	return (
 		<div className={styles.wrap}>
 			<div className={styles.avatar}>
-				<Image
+				{/* <Image
 					className={styles.image}
 					src="/otto.png" 
 					width={150}
 					height={190}
-					sizes="(max-width: 768px) 100vw"
 					alt="Picture of the author"
-				/>
+				/> */}
 			</div>
 			<div className={styles.name}>Otto Kustler</div>
 			<div className={styles.skills}>
