8cb757ad98d219e4c491b2d8d2796a5a1281f725
Webency <webency@mail.ru>
2023-08-23 13:28:09 +0300

    add link to social nedia

--- src/entities/SocialMedia/ui/SocialMedia.tsx
+++ src/entities/SocialMedia/ui/SocialMedia.tsx
@@ -1,5 +1,6 @@
 import cls from './SocialMedia.module.scss';
 import {classNames} from "@/shared/lib/classNames/classNames";
+import ExternalLink, { ThemeExternalLink } from '@/shared/ui/ExternalLink/ExternalLink';
 import Image from "next/image";
 interface SocialMediaProps {
     className?: string;
@@ -7,16 +8,24 @@ interface SocialMediaProps {
 
 export const SocialMedia = ({className = ""}:SocialMediaProps) => {
     const icons = [
-        {path: "/images/icons/github-icon.svg", link: "#", title: "github"},
-        {path: "/images/icons/telegram-icon.svg", link: "#", title: "telegram"},
-        {path: "/images/icons/discord-icon.svg", link: "#", title: "discord"},
-        {path: "/images/icons/twitter-icon.svg", link: "#", title: "twitter"},
+        {path: "/images/icons/github-icon.svg", link: "https://github.com/OpenDevCommune", title: "github"},
+        {path: "/images/icons/telegram-icon.svg", link: "https://t.me/+ts6hNf21bwU5N2Uy", title: "telegram"},
+        {path: "/images/icons/discord-icon.svg", link: "https://discord.gg/amzvJ7UTsz", title: "discord"},
+        {path: "/images/icons/twitter-icon.svg", link: "https://twitter.com/gybersocial", title: "twitter"},
     ]
     return (
         <div className={classNames(cls.SocialMedia, {}, [className])}>
             {icons.map(item => (
-                <Image key={item.title} src={item.path} alt={item.title} width={50} height={50} />
+                <ExternalLink 
+                    key={item.title} 
+                    href={item.link} 
+                    theme={ThemeExternalLink.CLEAR}
+                    target='_blank'
+                >
+                    <Image src={item.path} alt={item.title} width={50} height={50} />
+                </ExternalLink>
             ))}
+          
         </div>
     );
 };
