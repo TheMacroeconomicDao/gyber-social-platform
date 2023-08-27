516ef8bf43cee207385ed4c638c74beb8c28b012
Webency <webency@mail.ru>
2023-08-27 23:02:54 +0300

    fix social roadmap

--- src/widgets/SocialPlatformRoadmap/SPRoadmapping.tsx
+++ src/widgets/SocialPlatformRoadmap/SPRoadmapping.tsx
@@ -8,15 +8,18 @@ export const SPRoadmapping = () => {
   return (
     <Section background={ThemeBackgroundSection.SECOND}>
       <Container>
-        <div className={cls.wrapper}>
-          {data.map((item) => (
-            <SocialRoadmapCard
-              key={item.id}
-              id={item.id}
-              stages={item.stages}
-              description={item.description}
-            />
-          ))}
+        <div className={cls.Wrapper}>
+          <h2>Social Platform Roadmapping</h2>
+          <div className={cls.Roadmap}>
+            {data.map((item) => (
+              <SocialRoadmapCard
+                key={item.id}
+                id={item.id}
+                stages={item.stages}
+                description={item.description}
+              />
+            ))}
+          </div>
         </div>
       </Container>
     </Section>
