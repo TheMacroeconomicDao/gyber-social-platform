0c0cc158da0dd5b89c207cf070ffbc8b2cdc3600
Webency <webency@mail.ru>
2023-09-11 23:37:39 +0300

    decrease gap roadmap on mobile

--- src/entities/Roadmap/ui/RoadmapCard/lineItems/NextPeriodItem.tsx
+++ src/entities/Roadmap/ui/RoadmapCard/lineItems/NextPeriodItem.tsx
@@ -26,12 +26,12 @@ const NextPeriodItem = (props: ItemProps) => {
         <svg
           {...props}
           width="6"
-          height="80"
-          viewBox="0 0 6 80"
+          height="50"
+          viewBox="0 0 6 50"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <line x1="0" y1="0" x2="0" y2="80" stroke={props.color} strokeWidth="6" />
+          <line x1="0" y1="0" x2="0" y2="50" stroke={props.color} strokeWidth="6" />
         </svg>
       )}
     </>
