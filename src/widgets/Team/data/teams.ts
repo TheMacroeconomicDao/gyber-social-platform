e38fa22e2d9f861aac521dc449359a39d58a4879
Webency <webency@mail.ru>
2023-11-03 00:04:17 +0300

    extend team

--- src/widgets/Team/data/teams.ts
+++ src/widgets/Team/data/teams.ts
@@ -7,79 +7,85 @@ const data = [
     },
     {
         id: 2,
+        avatarSrc: "/images/teams/karina.jpg",
+        fullName: "Karina Yanyshevskaya",
+        skills: "lead-dev of AiC division, AI-engineer"
+    },
+    {
+        id: 3,
         avatarSrc: "/images/teams/Nikita-Juravski.jpg",
         fullName: "Nikita Juravski",
         skills: "Product manager, Backend Lead Developer"
     },
     {
-        id: 3,
+        id: 4,
         avatarSrc: "/images/teams/Pavel-Anisimov.jpg",
         fullName: "Pavel Anisimov",
         skills: "Head Designer, Community Manager"
     },
     {
-        id: 4,
+        id: 5,
         avatarSrc: "/images/teams/Wisdom-Forbes.jpg",
         fullName: "Wisdom Forbes",
         skills: "CMO, Community Lead Manager"
     },
     {
-        id: 5,
+        id: 6,
         avatarSrc: "/images/teams/Queen-Meries.jpg",
         fullName: "Queen Meries",
         skills: "PR and Community Manager"
     },
     {
-        id: 6,
+        id: 7,
         avatarSrc: "/images/teams/Young-Charles.jpg",
         fullName: "Young Charles",
         skills: "Media Manager"
     },
     {
-        id: 7,
+        id: 8,
         avatarSrc: "/images/teams/Fedor-Afanasiev.jpg",
         fullName: "Fedor Afanasiev",
         skills: "QA, Blockchain Developer"
     },
     {
-        id: 8,
+        id: 9,
         avatarSrc: "/images/teams/Artem-Smirnov.jpg",
         fullName: "Artem Smirnov",
         skills: "Architector, Lead Frontend Developer"
     },
     {
-        id: 9,
+        id:10,
         avatarSrc: "/images/teams/Daniil.jpg",
         fullName: "Daniil Borgoykov",
         skills: "AI Engineer"
     },
     {
-        id: 10,
+        id: 11,
         avatarSrc: "/images/teams/Dmitrii-Novikov.jpg",
         fullName: "Dmitrii Novikov",
         skills: "Software Architector"
     },
     
     {
-        id: 11,
+        id: 12,
         avatarSrc: "/images/teams/Bogdan.jpg",
         fullName: "Bogdan Yusupjanov",
         skills: "DevOps Engineer"
     },
     
     {
-        id: 12,
+        id: 13,
         avatarSrc: "/images/teams/Miracle-Obiekwe.jpg",
         fullName: "Miracle Obiekwe",
         skills: "Project Manager"
     },
     {
-        id: 13,
+        id: 14,
         avatarSrc: "/images/teams/Frank-Zulu.jpg",
         fullName: "Frank Zulu",
         skills: "Marketing officer"
     },
- 
+    
 ]
 
 export default data;
