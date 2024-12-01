ffdd06972d4257a07870a17d8a506a3d01ee6b94
Josiren <egor.novikov666@bk.ru>
2024-12-01 20:16:03 +0300

    teams: added Novikov Egor

--- src/widgets/Team/data/teams.ts
+++ src/widgets/Team/data/teams.ts
@@ -1,114 +1,118 @@
 const data = [
-    {
-        id: 1,
-        avatarSrc: "/images/teams/Giber.jpg",
-        fullName: "Lll CIlician",
-        skills: "Founder, Architector, CEO of GybernatyCommunity",
-        link: "https://t.me/themacroeconomicdao"
-    },
-    {
-        id: 2,
-        avatarSrc: "/images/teams/Karina.jpg",
-        fullName: "Karina Yanyshevskaya",
-        skills: "Lead-dev of AiC division, AI-engineer",
-        link: "https://t.me/fanoti"
-    },
-    {
-        id: 3,
-        avatarSrc: "/images/teams/Nikita-Juravski.jpg",
-        fullName: "Nikita Juravski",
-        skills: "Product manager, Backend Lead Developer"
-    },
-    {
-        id: 4,
-        avatarSrc: "/images/teams/Pavel-Anisimov.jpg",
-        fullName: "Pavel Anisimov",
-        skills: "Head Designer, Community Manager"
-    },
-    {
-        id: 5,
-        avatarSrc: "/images/teams/wisdom.jpg",
-        fullName: "Wisdom Forbes",
-        skills: "CMO, Community Lead Manager",
-        link: "https://t.me/ProfWisdomforbes"
-    },
-    {
-        id: 6,
-        avatarSrc: "/images/teams/Queen-Meries.jpg",
-        fullName: "Queen Meries",
-        skills: "PR and Community Manager"
-    },
-    {
-        id: 7,
-        avatarSrc: "/images/teams/Young-Charles.jpg",
-        fullName: "Young Charles",
-        skills: "Media Manager"
-    },
-    {
-        id: 8,
-        avatarSrc: "/images/teams/Fedor-Afanasiev.jpg",
-        fullName: "Fedor Afanasiev",
-        skills: "QA, Blockchain Developer"
-    },
-    {
-        id: 9,
-        avatarSrc: "/images/teams/Artem-Smirnov.jpg",
-        fullName: "Artem Smirnov",
-        skills: "Architector, Lead Frontend Developer",
-        link: "https://t.me/webency"
-    },
-    {
-        id:10,
-        avatarSrc: "/images/teams/Daniil.jpg",
-        fullName: "Daniil Borgoykov",
-        skills: "AI Engineer"
-    },
-    {
-        id: 11,
-        avatarSrc: "/images/teams/Dmitrii-Novikov.jpg",
-        fullName: "Dmitrii Novikov",
-        skills: "Software Architector"
-    },
-    
-    {
-        id: 12,
-        avatarSrc: "/images/teams/Bogdan.jpg",
-        fullName: "Bogdan Yusupjanov",
-        skills: "DevOps Engineer"
-    },
-    
-    {
-        id: 13,
-        avatarSrc: "/images/teams/Miracle-Obiekwe.jpg",
-        fullName: "Miracle Obiekwe",
-        skills: "Project Manager"
-    },
-    {
-        id: 14,
-        avatarSrc: "/images/teams/Frank-Zulu.jpg",
-        fullName: "Frank Zulu",
-        skills: "Marketing officer"
-    },
-    {
-        id: 15,
-        avatarSrc: "/images/teams/IrinaChekalina.jpg",
-        fullName: "Irina Chekalina",
-        skills: "Frontend Developer",
-        link: "https://t.me/irina_may_be"
-    },
-    {
-        id: 16,
-        avatarSrc: "/images/teams/ChimdaluNnenne.jpg",
-        fullName: "Chimdalu Nnenne (Fantasy)",
-        skills: "Content Marketer/Writer"
-    },
-    {
-        id: 17,
-        avatarSrc: "/images/teams/Kamran.jpg",
-        fullName: "Kamran Pulatov",
-        skills: "Backend Developer"
-    },
-    
-]
+  {
+    id: 1,
+    avatarSrc: "/images/teams/Giber.jpg",
+    fullName: "Lll CIlician",
+    skills: "Founder, Architector, CEO of GybernatyCommunity",
+    link: "https://t.me/themacroeconomicdao",
+  },
+  {
+    id: 2,
+    avatarSrc: "/images/teams/Karina.jpg",
+    fullName: "Karina Yanyshevskaya",
+    skills: "Lead-dev of AiC division, AI-engineer",
+    link: "https://t.me/fanoti",
+  },
+  {
+    id: 3,
+    avatarSrc: "/images/teams/Nikita-Juravski.jpg",
+    fullName: "Nikita Juravski",
+    skills: "Product manager, Backend Lead Developer",
+  },
+  {
+    id: 4,
+    avatarSrc: "/images/teams/Pavel-Anisimov.jpg",
+    fullName: "Pavel Anisimov",
+    skills: "Head Designer, Community Manager",
+  },
+  {
+    id: 5,
+    avatarSrc: "/images/teams/wisdom.jpg",
+    fullName: "Wisdom Forbes",
+    skills: "CMO, Community Lead Manager",
+    link: "https://t.me/ProfWisdomforbes",
+  },
+  {
+    id: 7,
+    avatarSrc: "/images/teams/Novikov-Egor.jpg",
+    fullName: "Novikov Egor",
+    skills: "Lead Frontend Developer, Web Designer",
+  },
+  {
+    id: 8,
+    avatarSrc: "/images/teams/Queen-Meries.jpg",
+    fullName: "Queen Meries",
+    skills: "PR and Community Manager",
+  },
+  {
+    id: 9,
+    avatarSrc: "/images/teams/Young-Charles.jpg",
+    fullName: "Young Charles",
+    skills: "Media Manager",
+  },
+  {
+    id: 10,
+    avatarSrc: "/images/teams/Fedor-Afanasiev.jpg",
+    fullName: "Fedor Afanasiev",
+    skills: "QA, Blockchain Developer",
+  },
+  {
+    id: 11,
+    avatarSrc: "/images/teams/Artem-Smirnov.jpg",
+    fullName: "Artem Smirnov",
+    skills: "Architector, Lead Frontend Developer",
+    link: "https://t.me/webency",
+  },
+  {
+    id: 12,
+    avatarSrc: "/images/teams/Daniil.jpg",
+    fullName: "Daniil Borgoykov",
+    skills: "AI Engineer",
+  },
+  {
+    id: 13,
+    avatarSrc: "/images/teams/Dmitrii-Novikov.jpg",
+    fullName: "Dmitrii Novikov",
+    skills: "Software Architector",
+  },
+  {
+    id: 14,
+    avatarSrc: "/images/teams/Bogdan.jpg",
+    fullName: "Bogdan Yusupjanov",
+    skills: "DevOps Engineer",
+  },
+
+  {
+    id: 15,
+    avatarSrc: "/images/teams/Miracle-Obiekwe.jpg",
+    fullName: "Miracle Obiekwe",
+    skills: "Project Manager",
+  },
+  {
+    id: 16,
+    avatarSrc: "/images/teams/Frank-Zulu.jpg",
+    fullName: "Frank Zulu",
+    skills: "Marketing officer",
+  },
+  {
+    id: 17,
+    avatarSrc: "/images/teams/IrinaChekalina.jpg",
+    fullName: "Irina Chekalina",
+    skills: "Frontend Developer",
+    link: "https://t.me/irina_may_be",
+  },
+  {
+    id: 18,
+    avatarSrc: "/images/teams/ChimdaluNnenne.jpg",
+    fullName: "Chimdalu Nnenne (Fantasy)",
+    skills: "Content Marketer/Writer",
+  },
+  {
+    id: 19,
+    avatarSrc: "/images/teams/Kamran.jpg",
+    fullName: "Kamran Pulatov",
+    skills: "Backend Developer",
+  },
+];
 
 export default data;
