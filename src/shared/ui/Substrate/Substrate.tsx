f3197fd2f5bc597a608d796f6fb9d5b558db1bf0
Fuad010 <dezhurik.com@gmail.com>
2025-03-08 11:14:46 +0400

    update

--- src/shared/ui/Substrate/Substrate.tsx
+++ src/shared/ui/Substrate/Substrate.tsx
@@ -1,3 +1,5 @@
+import { Button, ThemeButton } from "../Button/Button";
+import ExternalLink from "../ExternalLink/ExternalLink";
 import cls from "./Substrate.module.scss";
 import { classNames } from "@/shared/lib/classNames/classNames";
 interface SubstrateProps {
@@ -6,17 +8,28 @@ interface SubstrateProps {
   title?: string;
   subtitle?: string;
   text?: string;
+  buttonText?: string;
+  buttonLink?: string; 
 }
 
 export const Substrate = (props: SubstrateProps) => {
-  const { className = "", title, subtitle, text, scrollable = true } = props;
+  const { className = "", title, subtitle, text, scrollable = true, buttonText, buttonLink } = props;
   return (
-    <div className={classNames(cls.Substrate, {}, [className])}>
-      <h2>{title}</h2>
-      {subtitle && <h3>{subtitle}</h3>}
-      <div className={classNames(cls.text_box, { [cls.scroll]: scrollable })}>
-        {text}
+    <>
+      <div className={classNames(cls.Substrate, {[cls.with_button]: !!buttonText && !!buttonLink}, [className])}>
+        <h2>{title}</h2>
+        {subtitle && <h3>{subtitle}</h3>}
+        <div className={classNames(cls.text_box, {[cls.scroll]: scrollable,})}>
+          {text}
+        </div>
       </div>
-    </div>
+      {buttonText && buttonLink &&
+      <div className={cls.button_wrapper}>
+        <ExternalLink  href={buttonLink}>
+          <Button theme={ThemeButton.GREEN}>{buttonText}</Button>
+        </ExternalLink> 
+      </div>
+      }
+    </> 
   );
 };
