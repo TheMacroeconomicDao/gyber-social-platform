0ce163d7d9bce1ddd757fddb1b71f8dd912e6f52
Daler Abdurahimov <01.tolibovich@gmail.com>
2025-03-11 10:50:21 +0500

    change the telegram link from privat group to public group

--- src/widgets/JoinSection/JoinSection.tsx
+++ src/widgets/JoinSection/JoinSection.tsx
@@ -20,7 +20,7 @@ export const JoinSection = ({ className = "" }: JoinUsBlockProps) => {
                 <Line />
                 <div className={cls.buttonWrapper}>
                     <ExternalLink
-                        href="https://t.me/GybernatyCommunity"
+                        href="https://t.me/+ahBl6aq1PGtmMTFi"
                         theme={ThemeExternalLink.BLUE}
                         target="_blank"
                     >
