e8624b85e78bea84302279efeb5bbc0977066314
Fuad Topchiyev <dezhurik.com@gmail.com>
2025-03-26 15:54:26 +0400

    update

--- src/app/aic/page.tsx
+++ src/app/aic/page.tsx
@@ -39,16 +39,36 @@ const AIC = () => {
 }
 const data = {
   title: `The future of artificial intelligence: a study that opens up new horizons`,
-  text: `AiC (Artificial Intelligence and Blockchain Community) is a project whose goal is to create an open community that will develop and use artificial intelligence models in the blockchain environment. AiC plans to use DAO contracts to regulate the operation of the models and ensure transparency and openness in the development and use of the AI models we create.
+  text: `AiC (Artificial Intelligence and Blockchain Community) is a project that aims to create an open community of progressive AI researchers and developers with integration into Blockchain. AiC plans to use DAO contracts to regulate the operation of models and to ensure transparency and openness in the development and use of the AI models and technologies we create.
 
-            The problem that AiC solves is that the development and use of artificial intelligence models can be costly and complex, requiring significant resources and expertise. In addition, the use of models in different fields can raise concerns about privacy and security.
+    The problem that AiC solves is that the development and use of artificial intelligence models can be costly and complex, requiring significant resources and expertise. In addition, the use of models in different fields can raise concerns about privacy and security.
 
-            AiC proposes a solution to this problem by creating an open community where participants can collaboratively develop and use artificial intelligence models in the blockchain environment. The use of DAO contracts allows for transparency and openness in the process, as well as regulation of the operation of the artificial intelligence models.
-            The advantages of AiC lie in the fact that this open community allows participants to exchange knowledge and experience, as well as create and use artificial intelligence models in the blockchain environment with minimal costs. Additionally, the use of DAO contracts enables security and transparency in the process.
-            AiC can be used for both commercial and non-commercial purposes, such as creating artificial intelligence models in the fields of medicine, finance, transportation, and others.
+    AiC proposes a solution to this problem by creating an open community where participants can collaboratively develop and use artificial intelligence models in the blockchain environment. The use of DAO contracts allows for transparency and openness in the process, as well as regulation of the operation of the artificial intelligence models.
 
-            We are building a platform where participants can jointly create and use artificial intelligence models in the blockchain environment, which will help improve the efficiency of using these technologies and make them accessible to a wider audience.
-            `,
+    The advantages of AiC lie in the fact that this open community allows participants to exchange knowledge and experience, as well as create and use artificial intelligence models in the blockchain environment with minimal costs. Additionally, the use of DAO contracts enables security and transparency in the process.
+
+    AiC can be used for both commercial and non-commercial purposes, such as creating artificial intelligence models in the fields of medicine, finance, transportation, and others.
+
+    We are building a platform where participants can jointly create and use artificial intelligence models in the blockchain environment, which will help improve the efficiency of using these technologies and make them accessible to a wider audience.
+
+    AiC is an open community engaged in research and development of artificial intelligence models in the blockchain environment. We actively study and use all available libraries and frameworks for AI model development.
+
+
+    We utilizes DAO contracts to regulate the operation of our artificial intelligence models. DAO contracts enable network participants to make decisions about the development and use of AI models and regulate the distribution of rewards among participants.
+
+    Community is also actively exploring the possibilities of using machine learning technology to improve the functioning of blockchain networks and increase the efficiency of decentralized applications.
+
+    Thanks to our open approach and use of GPL licenses, we allow the wider community to contribute to the development and improvement of our AI models. We are confident that the joint efforts of community members will help us achieve great heights in the development of AI and blockchain technologies.
+
+    We are convinced that using these methods and technologies will allow us to create higher quality AI models and solve complex problems in various industries.
+
+    In a world where AI is becoming an increasingly important element of our lives, developers must be at the forefront of developing this technology. We urge all developers, from juniors to the most experienced, to join our community, which is developing AI for the benefit of society.
+
+    For juniors, this can be a great start to their career, where you can learn from us and gain experience working with our specialists. We are happy to share our knowledge and experience to help you become a sought-after developer in the future.
+
+    For more experienced developers, our community can be the perfect place to continue your development. You will be able to work together with our experts, apply the most advanced technologies, and participate in creating AI models that will be used in various industries.
+
+    Join our community and together we can make a huge contribution to the development of AI for society. By joining our platform, you will have access to the most advanced technologies, libraries, and frameworks for AI development. `,
   buttonText: `Join`,
   buttonLink: `https://t.me/+ahBl6aq1PGtmMTFi`,
 }
